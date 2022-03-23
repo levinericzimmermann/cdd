07540a9090ad9bbf17378df5dc98215136457b89
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-03-23 23:28:32 +0000

    Improve structure

---
+++ cdd/cdd/configurations/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+CHAPTER_TO_RENDER_TUPLE = tuple([])
+
+class Path(str):
+    def __setattr__(self, path_name: str, path: str):
+        path = f"{str(self)}/{path}"
+        try:
+            os.mkdir(path)
+        except FileExistsError:
+            pass
+        super().__setattr__(path_name, type(self)(path))
+
+
+PATH = Path(".")
+
+PATH.BUILDS = "builds"
+
+PATH.BUILDS.ILLUSTRATIONS = "illustrations"
+PATH.BUILDS.NOTATIONS = "notations"
+PATH.BUILDS.MIDI = "midi"
+PATH.BUILDS.SOUND_FILES = "sound_files"
+
+PATH.CDD = "cdd"
+PATH.CDD.DATA = "data"
+PATH.CDD.DATA.FADO = "fado"
+
+
+del os, Path
